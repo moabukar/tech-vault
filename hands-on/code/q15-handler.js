681def903dbe83ad7cd6068a99ac48c32c60e3d7
moabukar <mo-abukar10@hotmail.com>
2023-10-04 12:40:14 +0100

    add serverless question

---
+++ hands-on/code/q15-handler.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports.create = async (event) => {
+  // Your logic for CREATE
+};
+
+module.exports.get = async (event) => {
+  // Your logic for GET BY ID
+};
+
+module.exports.list = async (event) => {
+  // Your logic for LIST ALL
+};
+
+module.exports.update = async (event) => {
+  // Your logic for UPDATE
+};
+
+module.exports.delete = async (event) => {
+  // Your logic for DELETE
+};
